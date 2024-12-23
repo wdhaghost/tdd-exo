5d48d8e505356002332b7bae14b2d35d832bf045
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-23 11:56:54 +0100

    Ex 1 tdd calculator

---
+++ exo1/index.php
@@ -0,0 +1,13 @@
+<?php
+// Exercice 1 : Calculatrice
+// Implémentez une calculatrice avec les fonctionnalités suivantes :
+
+// Addition
+// Soustraction
+// Multiplication
+// Division
+// Étapes :
+
+// Écrivez un test pour chaque fonctionnalité.
+// Implémentez chaque méthode dans la classe Calculator pour faire passer les tests.
+
