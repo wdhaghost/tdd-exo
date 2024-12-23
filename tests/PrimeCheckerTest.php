e686aa040b106f3204be469c8af9b6e0e98c743c
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-23 15:27:33 +0100

    test to check if 17 is Prime

--- tests/PrimeCheckerTest.php
+++ tests/PrimeCheckerTest.php
@@ -8,9 +8,16 @@ class PrimeCheckerTest extends TestCase{
         $result = $primeChecker->isPrime(2);
         $this->assertTrue($result);
     }
+
     public function test_If_4_is_not_prime(){
         $primeChecker = new PrimeChecker();
         $result = $primeChecker->isPrime(4);
         $this->assertFalse($result);
+    }
+
+    public function test_If_17_is_prime(){
+        $primeChecker = new PrimeChecker();
+        $result = $primeChecker->isPrime(17);
+        $this->assertTrue($result);
     }
 }
